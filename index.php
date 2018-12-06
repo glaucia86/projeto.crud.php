79e9527e2dab8f16ed6ff5549af18071bcfcc2d3
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2018-12-06 03:49:02 -0200

    update bootstrap jquery js and html css edit

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="js/bootstrap.min.js"></script>
-    </head>
-    
-    <body>
-        <div class="jumbotron">
+
+<head>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Página Inicial</title>
+</head>
+
+<body>
         <div class="container">
+          <div class="jumbotron">
             <div class="row">
-                <h1> CRUD em PHP - Glaucia Lemos</h1>
+                <h2>CRUD em PHP <span class="badge badge-secondary">v 1.0.0 Glaucia</span></h2>
             </div>
+          </div>
             </br>
             <div class="row">
                 <p>
                     <a href="create.php" class="btn btn-success">Adicionar</a>
                 </p>
-                <table class="table table-striped table-bordered">
+                <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th>Nome</th>
-                            <th>Endereço</th>
-                            <th>Telefone</th>
-                            <th>Email</th>
-                            <th>Sexo</th>
-                            <th>Ação</th>
+                            <th scope="col">Id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Sexo</th>
+                            <th scope="col">Ação</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -33,30 +37,35 @@
                         include 'banco.php';
                         $pdo = Banco::conectar();
                         $sql = 'SELECT * FROM pessoa ORDER BY id DESC';
-                        
+
                         foreach($pdo->query($sql)as $row)
                         {
                             echo '<tr>';
+			                      echo '<th scope="row">'. $row['id'] . '</th>';
                             echo '<td>'. $row['nome'] . '</td>';
                             echo '<td>'. $row['endereco'] . '</td>';
                             echo '<td>'. $row['telefone'] . '</td>';
                             echo '<td>'. $row['email'] . '</td>';
                             echo '<td>'. $row['sexo'] . '</td>';
                             echo '<td width=250>';
-                            echo '<a class="btn btn-primary" href="read.php?id='.$row['id'].'">Listar</a>';
+                            echo '<a class="btn btn-primary" href="read.php?id='.$row['id'].'">Info</a>';
                             echo ' ';
                             echo '<a class="btn btn-warning" href="update.php?id='.$row['id'].'">Atualizar</a>';
                             echo ' ';
                             echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Excluir</a>';
                             echo '</td>';
-                            echo '<tr>';
+                            echo '</tr>';
                         }
                         Banco::desconectar();
                         ?>
-                    </tbody>                   
-                </table>               
+                    </tbody>
+                </table>
             </div>
         </div>
-        </div>
-    </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
 </html>
